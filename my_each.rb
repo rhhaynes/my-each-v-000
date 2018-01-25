366d7c8cc43d6df99c904035a603c1cfe63aee06
Randall Harris Haynes <harrishaynes@gmail.com>
2018-01-25 13:26:12 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,10 +1,14 @@
 def my_each(array)
-  i=0
-  while i<array.length
-    yield array[i]
-    i+=1
+  if block_given?
+    i=0
+    while i<array.length
+      yield array[i]
+      i+=1
+    end
+    array
+  else
+    puts "error: no block given."
   end
-  array
 end
 
 #collection = [1,2,3,4]
